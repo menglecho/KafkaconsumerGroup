7fd6f3832be1a30927adf9dfc0208f8f3b87b27f
Meng Liang <mengl.echo@gmail.com>
2018-05-08 23:27:11 +0200

    Update ConsumerGroup.js
    Add topic check

--- ConsumerGroup.js
+++ ConsumerGroup.js
@@ -1,25 +1,28 @@
 var kafka = require('kafka-node');
-
+var aalStore = require('../alarm-store/aalStore.js');
 var topic = ["AlarmStateIndication"];
-
 var options = {
     host: 'zookeeper:2181',
     kafkaHost: 'localhost:9092',
     groupId: 'adpalarmsnmpnbi',
     sessionTimeout: 30000,
     fromOffset: 'latest',
-    retries: 4
+    retries: 9
 };
 
 var consumerGroup = new kafka.ConsumerGroup(options, topic);
 
+consumerGroup.client.on('connect', function(){
+    consumerGroup.client.topicExists([topic], function(TopicsNotExistError){
+        console.log('Topic not exist ' + TopicsNotExistError);
+    });
+    console.log('connected');
+});
 consumerGroup.on('message', function (message) {
     console.log("kafka receive message!!");
-    console.log(message);
     aalStore.handleAlarmState(message);
 });
 
 consumerGroup.on('error', function (err) {
     console.error("Kafka server not ready " + err);
 });
-
